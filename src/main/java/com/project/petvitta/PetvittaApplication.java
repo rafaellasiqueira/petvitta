6a67e1ad654d907045e3fa7f13820bae9bd86938
rafaellasiqueira <rafaellafelipe2301@gmail.com>
2026-08-13 19:57:34 -0300

    Initial commit

---
+++ src/main/java/com/project/petvitta/PetvittaApplication.java
@@ -0,0 +1,14 @@
+package com.project.petvitta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Classe que irá inicializar o projeto
+@SpringBootApplication
+public class PetvittaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetvittaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/petvitta/PetvittaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.petvitta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetvittaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
